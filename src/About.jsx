d6381518582f09965557d098f05e6ca3939482a1
jon.lasley@gmail.com <jon.lasley@gmail.com>
2021-02-15 19:08:20 -0700

    Looking really good

--- src/About.jsx
+++ src/About.jsx
@@ -5,14 +5,13 @@ const About = () => {
   const [showInfo, toggleShowInfo] = useState(false);
 
   return (
-    <div id="about" className="about">
-      <div onClick={() => toggleShowInfo(!showInfo)}>Info {showInfo ? '↓' : '↑'}
+    <div className="about">
+      <div id="about" onClick={() => toggleShowInfo(!showInfo)}>Info {showInfo ? '↓' : '↑'}      </div>
         <CSSTransition in={showInfo} timeout={500} classNames="show-info">
           <div className="about-info" style={{display: showInfo ? "block" : "none"}}>
             Displays all near-earth-objects for a given day, as retrieved from NASA. Change the day to see historical records. Hover an asteroid's name on the right to see more info. Asteroid distance, size, and orbital speed are logarithmically proportional to the actual data. Red dots are "potentially hazardous" asteroids.
               </div>
         </CSSTransition>
-      </div>
     </div>
   )
 }

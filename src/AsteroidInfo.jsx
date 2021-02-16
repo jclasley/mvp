44160739aa576ff54f8dd64038a11afa6c6c4f05
jon.lasley@gmail.com <jon.lasley@gmail.com>
2021-02-15 19:20:17 -0700

    Fade in for text

--- src/AsteroidInfo.jsx
+++ src/AsteroidInfo.jsx
@@ -1,4 +1,5 @@
 import React, {useState} from 'react';
+import {CSSTransition} from 'react-transition-group';
 
 const AsteroidInfo = ({time, distance, velocity, size, name, select}) => {
   const [isHover, setHover] = useState(false);
@@ -8,10 +9,7 @@ const AsteroidInfo = ({time, distance, velocity, size, name, select}) => {
     for (let circle of circles) {
       circle.id !== name && circle.setAttribute('style', 'opacity: 0.25;')
     }
-    const form = document.getElementsByTagName('form');
-    for (let f of form) {
-      f.setAttribute('style', 'opacity: 0.25;');
-    }
+
     document.getElementById(`${name}-outline`).setAttribute('stroke', 'white')
     document.getElementById(`${name}-outline`).setAttribute('style', 'opacity: 1;')
   }
@@ -21,10 +19,6 @@ const AsteroidInfo = ({time, distance, velocity, size, name, select}) => {
     for (let circle of circles) {
       circle.setAttribute('style', 'opacity: 1;')
     }
-    const form = document.getElementsByTagName('form');
-    for (let f of form) {
-      f.setAttribute('style', 'opacity: 1;');
-    }
     document.getElementById(`${name}-outline`).setAttribute('stroke', 'none')
   }
   return (
@@ -32,15 +26,17 @@ const AsteroidInfo = ({time, distance, velocity, size, name, select}) => {
       <div className="asteroidList" 
         style={{color: isHover ? "lightblue" : "white", opacity: isHover ? 1 : 0.5}}
         onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>{name}</div>
-      {isHover && (
-        <div className="asteroidInfo">
+
+      <CSSTransition in={isHover} timeout={1000} classNames="show-info">
+        <div style={{display: isHover ? 'block' : 'none'}} className="asteroidInfo">
           <p className="name">{name}</p>
           <p>Diameter: {((size.kilometers.max + size.kilometers.min) / 2).toFixed(2)} km</p>
           <p>Distance: {distance.km.toFixed(2)} km</p>
           <p>Velocity: {velocity.kph.toFixed(2)} kph</p>
           <p>Closest to Earth: {time} UTC</p>
         </div>
-      )}
+    </CSSTransition>
+      
     </>
   )
 }

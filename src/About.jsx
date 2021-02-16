7c62203e3222a490375dc585df65960dace40a28
jon.lasley@gmail.com <jon.lasley@gmail.com>
2021-02-15 19:21:21 -0700

    Fixed fade

--- src/About.jsx
+++ src/About.jsx
@@ -7,11 +7,9 @@ const About = () => {
   return (
     <div className="about">
       <div id="about" onClick={() => toggleShowInfo(!showInfo)}>Info {showInfo ? '↓' : '↑'}      </div>
-        <CSSTransition in={showInfo} timeout={500} classNames="show-info">
-          <div className="about-info" style={{display: showInfo ? "block" : "none"}}>
-            Displays all near-earth-objects for a given day, as retrieved from NASA. Change the day to see historical records. Hover an asteroid's name on the right to see more info. Asteroid distance, size, and orbital speed are logarithmically proportional to the actual data. Red dots are "potentially hazardous" asteroids.
-              </div>
-        </CSSTransition>
+        <div className="about-info" style={{display: showInfo ? "block" : "none"}}>
+          Displays all near-earth-objects for a given day, as retrieved from NASA. Change the day to see historical records. Hover an asteroid's name on the right to see more info. Asteroid distance, size, and orbital speed are logarithmically proportional to the actual data. Red dots are "potentially hazardous" asteroids.
+        </div>
     </div>
   )
 }
